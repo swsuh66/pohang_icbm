a42f23b82771a6a92437870ce3902804c8bfb323
hoseck <hoseck72@hotmail.com>
2025-05-18 18:05:13 +0900

    누수의심알림 동의 및 조회 API 생성

--- src/main/java/com/istec/m1/dto/AlertDto.java
+++
@@ -1,9 +0,0 @@
-package com.istec.m1.dto;
-
-import lombok.Data;
-@Data
-public class AlertDto {
-    private String userId; // 수용가번호
-    private String userName; // 수용가명
-    private String userPhone; // 수용가전화번호
-}
